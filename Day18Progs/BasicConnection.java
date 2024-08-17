144348932e19740c631a5ab09ce0c914f453216f
Aradhya Pundir <Aradhya.Pundir@godigit.com>
2024-08-17 16:13:46 +0530

    [ARADHYA] Add: Adding day 18 Progs

---
+++ Day18Progs/BasicConnection.java
@@ -0,0 +1,19 @@
+package Day18Progs;
+
+import java.sql.*;
+import java.io.*;
+public class BasicConnection
+{
+    public static void main(String[] args)
+    {
+        String url="jdbc:postgresql://localhost:5432/test_db";
+        String userName="postgres";
+        String password="root";
+        try {
+            Connection con=DriverManager.getConnection(url,userName,password);
+            System.out.println("connection established");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
